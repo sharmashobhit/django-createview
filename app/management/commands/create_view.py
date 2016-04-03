23ea320fdd5b7b6d79fe6dd6d30ccc2425755ebe
Shobhit Sharma <shobhit@coverfoxmail.com>
2016-04-03 19:47:58 +0530

    Created base views' code

--- app/management/commands/create_view.py
+++ app/management/commands/create_view.py
@@ -1,6 +1,16 @@
 from django.core.management.base import BaseCommand, CommandError
 import importlib
 import os
+import re
+import inspect
+
+IMPORTS = {
+        "CreateView": "django.views.generic.edit",
+        "FormView": "django.views.generic.edit",
+        "UpdateView": "django.views.generic.edit",
+        "DeleteView": "django.views.generic.edit",
+        "TemplateView": "django.views.generic.base",
+        }
 
 
 class Command(BaseCommand):
@@ -18,34 +28,41 @@ class Command(BaseCommand):
             help='Delete poll instead of closing it')
 
     def handle(self, *args, **options):
-        print args
-        print options
         view_path = options['view_name'][0].split('.')
-        view_name = view_path[-1]
+        self.view_name = view_path[-1]
         try:
             module = importlib.import_module('.'.join(view_path[:-1]))
         except ImportError:
             raise
-        view_exists = hasattr(module, view_name)
+        view_exists = hasattr(module, self.view_name)
         view_type = options['view_type']
         if not view_exists:
-            with open('/'.join(view_path[:-1])+".py", "a") as myfile:
-                myfile.write(getattr(self, view_type)(*args, **options))
+            with open(inspect.getsourcefile(module), "a") as myfile:
+                myfile.write("\n\n")
+                if not hasattr(module, view_type) and IMPORTS.get(view_type) is not None:
+                    myfile.write("### AUTOMATED IMPORT. MOVE THIS TO TOP OF THE FILE###\n")
+                    myfile.write("from {0} import {1}\n\n".format(IMPORTS[view_type], view_type))
+                myfile.write(getattr(self, view_type)())
         else:
-            raise CommandError('View "{0}" already exists'.format(view_name))
+            raise CommandError('View "{0}" already exists'.format(self.view_name))
         print "Success"
 
-    def CreateView(self, *args, **options):
-        assert False, options['view_type']
+    def CreateView(self):
+        return_data = "class {0}(CreateView):"+\
+        "\n\ttemplate_name = '{0}.html'"+\
+        "\n\tsuccess_url = '<success_url>'"+\
+        "\n\t# form_class = <SubstituteYourForm>"
+        return return_data.format(self.view_name)
 
-    def fbv(self, *args, **kwargs):
+    def fbv(self):
         return "def {0}(request):\
         \n\ttemplate_name='{0}.html'\
-        \n\treturn render(template_name)"
+        \n\treturn render(template_name)".format(self.view_name)
 
-    def TemplateView(self, *args, **kwargs):
-        return "class {0}(TemplateView):\
-        \n\ttemplate_name='{0}.html'\
-        \n\n\tdef get_context_data(self, **kwargs):\
-        \n\t\tcontext = super({0}, self).get_context_data(**kwargs)\
-        \n\t\treturn context"
+    def TemplateView(self):
+        return_data = "class {0}(TemplateView):"+\
+        "\n\ttemplate_name='{0}.html'"+\
+        "\n\n\tdef get_context_data(self, **kwargs):"+\
+        "\n\t\tcontext = super({0}, self).get_context_data(**kwargs)"+\
+        "\n\t\treturn context"
+        return return_data.format(self.view_name)
--- app/views.py
+++ app/views.py
@@ -3,3 +3,17 @@ from django.shortcuts import render
 # Create your views here.
 def SampleView(request):
     return True
+
+
+### AUTOMATED IMPORT. MOVE THIS TO TOP OF THE FILE###
+from django.views.generic.edit import CreateView
+
+class BlahView1(CreateView):
+	template_name = 'BlahView1.html'
+	success_url = '<success_url>'
+	# form_class = <SubstituteYourForm>
+
+class 12BlahView1(CreateView):
+	template_name = '12BlahView1.html'
+	success_url = '<success_url>'
+	# form_class = <SubstituteYourForm>
